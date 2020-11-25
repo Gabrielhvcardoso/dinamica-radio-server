99e3223f80534b42fe5b73e785dd2b16d4d8f54a
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-11-25 10:25:07 -0300

    find client

--- src/functions/core/client/index.ts
+++ src/functions/core/client/index.ts
@@ -1,10 +1,12 @@
 import index from './core'
+import destroy from './core/destroy'
+import find from './core/find'
 import store from './core/store'
 import update from './core/update'
-import destroy from './core/destroy'
 
 export default {
   index,
+  find,
   store,
   update,
   destroy
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -6,6 +6,7 @@ type Response = { code: string }
 interface Functions {
   client: {
     index: () => Promise<Array<Client>>,
+    find: (clientId: number) => Promise<Array<Client> | Response>,
     store: (mail: string, password: string) => Promise<number | Response>,
     update: (clientId: number, mail: string, password: string) => Promise<Response>,
     destroy: (clientId: number) => Promise<Response>
