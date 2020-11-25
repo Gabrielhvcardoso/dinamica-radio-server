c999d56c143c80e0817447140f3dba1def8a503c
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-11-25 14:43:34 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
+// require('dotenv').config()
 
 module.exports = {
   client: 'mysql',
   version: '5.5',
   connection: {
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD
+    host: 'mysql.dmihost.com.br',
+    database: 'dmihost01',
+    user: 'dmihost01',
+    password: 'PmdFbgl4zo2z'
   }
 }
