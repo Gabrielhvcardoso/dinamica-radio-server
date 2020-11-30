a95e184ed1e7fe1833d1eb32bf9bcc9df4f665d0
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-11-30 15:04:43 -0300

    order on index schedule programs

--- src/api/scheduleProgram/core/index.ts
+++ src/api/scheduleProgram/core/index.ts
@@ -1,36 +1,25 @@
 import knex from '../../../database'
 
-function resolveWeekday (intWeekday: number) {
-  switch (intWeekday) {
-    case 1: return 'sunday'
-    case 2: return 'monday'
-    case 3: return 'tuesday'
-    case 4: return 'wednesday'
-    case 5: return 'thursday'
-    case 6: return 'friday'
-    default: return 'saturday'
-  }
-}
-
 export default async function index (req, res) {
   const schedulePrograms = await knex('scheduleProgram')
     .innerJoin('program', 'scheduleProgram.programId', 'program.programId')
     .select('program.programId', 'weekday', 'startAt', 'duration', 'title', 'image')
 
-  const reduced = schedulePrograms.reduce((acc, item) => {
-    const weekday = resolveWeekday(item.programId)
-    acc[weekday].push(item)
+  const sunday = schedulePrograms.filter((item) => item.weekday === 1).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const monday = schedulePrograms.filter((item) => item.weekday === 2).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const tuesday = schedulePrograms.filter((item) => item.weekday === 3).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const wednesday = schedulePrograms.filter((item) => item.weekday === 4).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const thursday = schedulePrograms.filter((item) => item.weekday === 5).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const friday = schedulePrograms.filter((item) => item.weekday === 6).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
+  const saturday = schedulePrograms.filter((item) => item.weekday === 7).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
 
-    return acc
-  }, {
-    sunday: [],
-    monday: [],
-    tuesday: [],
-    wednesday: [],
-    thursday: [],
-    friday: [],
-    saturday: []
+  res.send({
+    sunday,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday
   })
-
-  res.send(reduced)
 }
