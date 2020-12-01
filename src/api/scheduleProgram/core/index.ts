85f7e8c02bd2f9431739c386f74b941fb7c76ad0
gabrielhvcardoso <gabrielhvcardoso@gmail.com>
2020-12-01 09:51:24 -0300

    add hash string to index schedule program

--- src/api/scheduleProgram/core/index.ts
+++ src/api/scheduleProgram/core/index.ts
@@ -1,9 +1,15 @@
+import crypto from 'crypto'
 import knex from '../../../database'
 
 export default async function index (req, res) {
-  const schedulePrograms = await knex('scheduleProgram')
+  const ReceivedSchedulePrograms = await knex('scheduleProgram')
     .innerJoin('program', 'scheduleProgram.programId', 'program.programId')
     .select('program.programId', 'weekday', 'startAt', 'duration', 'title', 'image')
+
+  const schedulePrograms = ReceivedSchedulePrograms.map((item) => ({
+    ...item,
+    hash: crypto.randomBytes(16).toString('hex')
+  }))
 
   const sunday = schedulePrograms.filter((item) => item.weekday === 1).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
   const monday = schedulePrograms.filter((item) => item.weekday === 2).sort((a, b) => a.startAt > b.startAt ? 1 : -1).map((item, index) => ({ ...item, order: index }))
